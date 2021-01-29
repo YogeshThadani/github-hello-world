45225f34601c9291a6d48b0becce13b16a78d990
Yogesh <43539665+YogeshThadani@users.noreply.github.com>
2021-01-29 15:46:58 +0530

    Intial Commit of math.py

---
+++ math.py
@@ -0,0 +1,18 @@
+#MATHEMATICAL
+def add(x, y):
+	return (x+y)
+
+#SUb
+def subtract(x, y):
+	if x > Y:
+		return "error"
+	return x-y
+
+#div
+def divide(x, y):
+	pass
+
+#mul
+def multiply(x, y):
+	pass
+
